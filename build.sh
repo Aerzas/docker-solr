05b7256ae7fc653d609a65e8ac144ecb105fcc11
Aerzas <nicolas.ricklin@protonmail.com>
2022-02-14 09:19:54 +0100

    Bump Solr version

--- build.sh
+++ build.sh
@@ -18,15 +18,15 @@ solr_version() {
   fi
 
   case ${solr_version} in
-  7.7)
+  7)
     SOLR_VERSION="7.7.3"
     SOLR_SHA512="ca9200c18cc19ab723fd4d10f257e27eb81dc8bc33401ebc4eb99178faf4033a2684f0f8b12ae7b659cfeb0f4c9d9e24aaac518a4e00fd28b69854a359a666ed"
     SOLR_KEYS="CFCE5FBB920C3C745CEEE084C38FF5EC3FCFDB3E"
     ;;
-  8.5)
-    SOLR_VERSION="8.5.2"
-    SOLR_SHA512="02b9b90468f399701dba26695c9af6cd205f47916a06e26838613fe238594e9902de6ef3b42ec8257d195e37589adf8427d9b7962557731e91949fbef06bb544"
-    SOLR_KEYS="86EDB9C33B8517228E88A8F93E48C0C6EF362B9E"
+  8)
+    SOLR_VERSION="8.11.1"
+    SOLR_SHA512="4893f836aec84b03d7bfe574e59e305c03b5ede4a48020034fbe81440b8feee79e55fd9ead230e5b89b3f25124e9b56c1ddc4bb5c7f631cf4e846b9cab5f9a45"
+    SOLR_KEYS="2CECBFBA181601547B654B9FFA81AC8A490F538E"
     ;;
   *)
     return 1
@@ -70,6 +70,6 @@ if [ -n "${build_solr_version}" ]; then
   build_solr "${build_solr_version}"
 # Build all SOLR versions
 else
-  build_solr 7.7
-  build_solr 8.5
+  build_solr 7
+  build_solr 8
 fi
